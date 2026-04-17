91f619e16a5710a8cf5a57988fa3afa250641ad4
Dave Dykes <148535937+brightworkdigital@users.noreply.github.com>
2026-04-17 14:28:20 +0100

    Second commit I just learned about 😬

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+        double result = Calculator.add(3.0, 0.14);
+
+        System.out.println(result);
+    }
 }
